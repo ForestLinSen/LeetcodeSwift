8992d58e5c46a61639ecc58259485dfdc0e30570
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2021-09-18 18:32:46 +0800

    performance improved
    Using enumerate to improve the performance

--- twoSums/twoSums.playground/Contents.swift
+++ twoSums/twoSums.playground/Contents.swift
@@ -1,32 +1,44 @@
 import Cocoa
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    var answer = [Int]()
-    
     var record: [Int: Int] = [:]
     
-    for i in 0...nums.count-1{
-        record[nums[i]] = i
-    }
-    
-    for i in 0...nums.count-1{
-        let currentNum = nums[i]
-        let dif = target - currentNum
-
-        let searchValue = record[dif]
+    for (i, currentNumber) in nums.enumerated(){
+        let dif = target - currentNumber
         
+        let searchValue = record[dif]
         if let index = searchValue{
-            
-            if(index != i){
-                answer.append(i)
-                answer.append(index)
-                return answer
+            if (index != i){
+                return [i, record[dif]!]
             }
-
         }
+        
+        record[currentNumber] = i
     }
     
-    return answer
+    return [-1, -1]
+//    for i in 0...nums.count-1{
+//        record[nums[i]] = i
+//    }
+//
+//    for i in 0...nums.count-1{
+//        let currentNum = nums[i]
+//        let dif = target - currentNum
+//
+//        let searchValue = record[dif]
+//
+//        if let index = searchValue{
+//
+//            if(index != i){
+//                answer.append(i)
+//                answer.append(index)
+//                return answer
+//            }
+//
+//        }
+//    }
+    
+    
 }
 
 let array = [-1,-2,-3,-4,-5]
