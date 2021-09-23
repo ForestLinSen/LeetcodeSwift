f7c082b2f8a25fdabedc70ff7eda2e83dc45eac9
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2021-09-23 13:05:49 +0800

    9.1 add solution version 2 of this problem

--- 9_palindrome_number/palindrome_number.playground/Contents.swift
+++ 9_palindrome_number/palindrome_number.playground/Contents.swift
@@ -1,31 +1,53 @@
 import Cocoa
 
+// version 2:
 func isPalindrome(_ x: Int) -> Bool {
-    let a:[Character] = Array(String(x))
     
-    if(a.count <= 1){
-        return true
+    if x < 0 {
+        return false
     }
     
-    var left = 0
-    var right = a.count - 1
+    let x = x
+    var result = 0
+    var divisor = 1
     
-    while(left <= right){
-        
-        print("left: \(left), right: \(right)")
-        
-        if(a[left] == a[right]){
-            left += 1
-            right -= 1
-        }else{
-            return false
-        }
+    while x/divisor != 0{
+        result *= 10
+        result += x / divisor % 10
+        divisor *= 10
+        print(result)
     }
     
-    return true
+    return result == x
 }
 
-isPalindrome(-101)
+isPalindrome(1234321)
 
-
-121 / 10 % 10
+// version 1: using array
+//func isPalindrome(_ x: Int) -> Bool {
+//    let a:[Character] = Array(String(x))
+//
+//    if(a.count <= 1){
+//        return true
+//    }
+//
+//    var left = 0
+//    var right = a.count - 1
+//
+//    while(left <= right){
+//
+//        print("left: \(left), right: \(right)")
+//
+//        if(a[left] == a[right]){
+//            left += 1
+//            right -= 1
+//        }else{
+//            return false
+//        }
+//    }
+//
+//    return true
+//}
+//
+//isPalindrome(-101)
+//
