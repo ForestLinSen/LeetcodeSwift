53630bb8481bc649720bfbbf6a47cb026556699f
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2021-09-24 15:38:38 +0800

    add gridTraveler problem

--- Dynamic_programming/dynamic_programming.playground/Contents.swift
+++ Dynamic_programming/dynamic_programming.playground/Contents.swift
@@ -3,9 +3,10 @@ import Cocoa
 
 // Part 1: Memorization
 
+// 1.1 Fibonacci
 struct Dynamic_fib{
     
-    // 1.1 fibonacci number: normal recurrsion
+    // 1.1.1 fibonacci number: normal recurrsion
     /// Time complexity: O(2^N)
     static func normal_fib(_ n:Int) -> Int{
         guard n > 0 else {return -1}
@@ -21,7 +22,7 @@ struct Dynamic_fib{
         /// fib(n-1) + fib(n-2)
         /// lib(n-2) + lib(n-2)
     
-    // 1.2 Dynamic fibonacci
+    // 1.1.2 Dynamic fibonacci
     static var memo  = [Int:Int]()
     
     static func dynamic_fib(_ n: Int) -> Int{
@@ -42,6 +43,47 @@ struct Dynamic_fib{
 
 
 Dynamic_fib.dynamic_fib(50)
-//Dynamic_fib.normal_fib(50)
+
+
+// 1.2 gridTraveler
+/// down -> func(2, 3) becomes (1, 3)
+/// right -> func(2, 3) becomes (2, 2)
+
+struct Dynamic_gridTraveler{
+    static func normal_gridTraveler(_ m: Int, _ n: Int) -> Int{
+        
+        if(m == 0 || n == 0){
+            return 0
+        }else if(m == 1 && n == 1){
+            return 1
+        }
+        
+        return normal_gridTraveler(m-1, n) + normal_gridTraveler(m, n-1)
+    }
+    
+    static var memo = [String: Int]()
+    
+    static func dynamic_gridTraveler(_ m: Int, _ n: Int) -> Int{
+        
+        if(m == 0 || n == 0){
+            return 0
+        }else if(m == 1 && n == 1){
+            return 1
+        }
+        
+        let key = String(m) + "," + String(n)
+        
+        if let num = memo[key]{
+            return num
+        }else{
+            memo[key] = dynamic_gridTraveler(m-1, n) + dynamic_gridTraveler(m, n-1)
+            return memo[key]!
+        }
+    }
+}
+
+Dynamic_gridTraveler.dynamic_gridTraveler(42,20)
+//Dynamic_gridTraveler.normal_gridTraveler(20,20)
+
 
 // Part 2: Tabulation
