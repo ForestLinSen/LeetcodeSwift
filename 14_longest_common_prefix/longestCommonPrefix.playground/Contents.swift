8b6ec5db8a92dd210092cc405275feb10a5b5a98
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2021-10-05 15:50:00 +0800

    longest common prefix v2

--- 14_longest_common_prefix/longestCommonPrefix.playground/Contents.swift
+++ 14_longest_common_prefix/longestCommonPrefix.playground/Contents.swift
@@ -1,30 +1,48 @@
 import Cocoa
 
-
-// v1
-func longestCommonPrefix(_ strs: [String]) -> String {
+func longestCommonPrefix(_ strs: [String]) -> String{
     if(strs.count == 1){return strs[0]}
     
-    var result = ""
-    var index = 1
+    var prefix = strs[0]
     
-    while index <= strs[0].count{
-        
-        let prefix = strs[0].prefix(index)
-        
-        for s in strs{
-            if s.hasPrefix(prefix){
-                continue
-            }else{
-                return result
-            }
+    for i in 1..<strs.count{
+        while !strs[i].hasPrefix(prefix) {
+            prefix.removeLast()
         }
-        
-        index += 1
-        result = String(prefix)
+        if prefix.count == 0{
+            return ""
+        }
     }
     
-    return result
+    return prefix
 }
 
-longestCommonPrefix(["flower","flower","flower","flower"])
+longestCommonPrefix(["flower","flow","flight"])
+
+// v1
+//func longestCommonPrefix(_ strs: [String]) -> String {
+//    if(strs.count == 1){return strs[0]}
+//
+//    var result = ""
+//    var index = 1
+//
+//    while index <= strs[0].count{
+//
+//        let prefix = strs[0].prefix(index)
+//
+//        for s in strs{
+//            if s.hasPrefix(prefix){
+//                continue
+//            }else{
+//                return result
+//            }
+//        }
+//
+//        index += 1
+//        result = String(prefix)
+//    }
+//
+//    return result
+//}
+
+
