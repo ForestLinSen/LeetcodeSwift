2c0996679795954dda3bf43480363c2b457ad890
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-01-05 23:32:08 +0800

    a better method

--- 19_remove_nth_node/removeNthNode.playground/Contents.swift
+++ 19_remove_nth_node/removeNthNode.playground/Contents.swift
@@ -37,48 +37,28 @@ public class ListNode{
 class Solution {
     func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
         
-        guard var head = head, head.next != nil else{
-            return nil
+        let dummy = ListNode(-1)
+        dummy.next = head
+        
+        var curr: ListNode? = dummy
+        var nth: ListNode? = dummy
+        
+        for _ in 0..<n{
+            curr = curr?.next
+            guard curr != nil else { return head}
         }
         
-        var i = 0
-        var j = 0
-        
-        var newHead: ListNode = head
-        while(newHead.next != nil){
-            newHead = newHead.next!
-            i+=1
+        while curr?.next != nil{
+            nth = nth?.next
+            curr = curr?.next
         }
         
-        print("index i: \(i)")
+        nth?.next = nth?.next?.next
         
-        let targetIndex = i-n
-        print("target index: \(targetIndex)")
-        
-        if(targetIndex < 0){
-            guard let head = head.next else{
-                return nil
-            }
-            return head
-        }
-        
-        var secondHead: ListNode = head
-        while(j != targetIndex){
-            secondHead = secondHead.next!
-            j+=1
-        }
-        
-        print("index j: \(j)")
-        
-        // Delete the node
-        let toDeleteNode = secondHead.next
-        
-        secondHead.next = toDeleteNode?.next
-        
-        print(toDeleteNode?.val)
-        return head
+        return dummy.next
         
     }
 }
+
 
 let s = Solution()
