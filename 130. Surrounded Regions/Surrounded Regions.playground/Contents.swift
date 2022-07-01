c8072cc411ec882c8929d6a6e9e3a891bd7a59ee
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-01 21:46:25 +0800

    Surrounded Regions solution 2

--- 130. Surrounded Regions/Surrounded Regions.playground/Contents.swift
+++ 130. Surrounded Regions/Surrounded Regions.playground/Contents.swift
@@ -2,33 +2,56 @@
 
 class Solution {
     func solve(_ board: inout [[Character]]) {
-        for r in board.indices{
-            for c in board[r].indices where board[r][c] == "O"{
-                var curr = board
-                if dfs(&curr, r, c){
-                    board = curr
+        let rows = board.count
+        let columns = board[0].count
+        
+        guard rows >= 3, columns >= 3 else { return }
+        
+        func capture(_ row: Int, _ column: Int){
+            if row < 0 || row >= rows || column < 0 || column >= columns || board[row][column] != "O"{
+                return
+            }
+            
+            board[row][column] = "T"
+            
+            // using the mark symbol "T"
+            let deltas: [(Int, Int)] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            
+            for delta in deltas {
+                let newRow = row + delta.0
+                let newColumn = column + delta.1
+                capture(newRow, newColumn)
+            }
+        }
+        
+        // 1. (DFS) Capture unsurrounded regions (O -> T)
+        for column in 0..<columns{
+            capture(0, column)
+            capture(rows-1, column)
+        }
+        
+        for row in 0..<rows{
+            capture(row, 0)
+            capture(row, columns-1)
+        }
+        
+        // 2. Capture surrounded regions (O -> X)
+        for row in 1..<(rows-1){
+            for column in 1..<(columns-1){
+                if board[row][column] == "O"{
+                    board[row][column] = "X"
                 }
             }
         }
-    }
-    
-    
-    private func dfs(_ board: inout [[Character]], _ r: Int, _ c: Int) -> Bool{
-        if r < 0 || r >= board.count || c < 0 || c >= board[r].count { return false }
         
-        // base case
-        if board[r][c] == "X" { return true }
-        
-        board[r][c] = "X"
-        
-        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]{
-            let nr = r + dx
-            let nc = c + dy
-            if !dfs(&board, nr, nc){
-                return false
+        // 3. Uncapture surrounded regions (T -> O)
+        for row in 0..<rows{
+            for column in 0..<columns{
+                if board[row][column] == "T"{
+                    board[row][column] = "O"
+                }
             }
         }
-        return true
     }
 }
 
