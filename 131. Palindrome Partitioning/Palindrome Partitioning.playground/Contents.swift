0f1ed1c7f37e7b3e27e96fda47fcd7a515fe9da5
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-16 18:21:12 +0800

    palindrome

---
+++ 131. Palindrome Partitioning/Palindrome Partitioning.playground/Contents.swift
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/palindrome-partitioning/description/
+
+class Solution {
+    func partition(_ s: String) -> [[String]] {
+        var results: [[String]] = []
+        
+        func backtracking(_ curr: [String], _ start: Int){
+            
+        }
+        
+        
+    }
+}
