2c80f7595421a596bf8f85ef339e41be19d40d1f
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-21 23:33:15 +0800

    palindrome partition review

--- 131. Palindrome Partitioning/Palindrome Partitioning.playground/Contents.swift
+++ 131. Palindrome Partitioning/Palindrome Partitioning.playground/Contents.swift
@@ -1,44 +1,89 @@
 // https://leetcode.com/problems/palindrome-partitioning/description/
-
 class Solution {
     func partition(_ s: String) -> [[String]] {
         var results: [[String]] = []
-        var curr: [String] = []
-        doPartition(&curr, &results, Array(s))
-        return results
-
-    }
-    
-    private func doPartition(_ curr: inout [String], _ results: inout [[String]], _ characters: [Character]){
-        if characters.count == 0{
-            results.append(curr)
-            return
-        }
         
-        for i in 0..<characters.count{
-            if isPalindrome(characters, 0, i){
-                curr.append(String(characters[0..<i+1]))
-                doPartition(&curr, &results, Array(characters[(i+1)...]))
-                curr.removeLast()
+        func backtracking(_ curr: inout [String], _ characters: [Character]){
+            if characters.count == 0{
+                results.append(curr)
+                return
+            }
+            
+            for i in 0..<characters.count{
+                if isPalindrome(characters, 0, i){
+                    curr.append(String(characters[0...i]))
+                    backtracking(&curr, Array(characters[(i+1)...]))
+                    curr.removeLast()
+                }
             }
         }
+        
+        var curr: [String] = []
+        backtracking(&curr, Array(s))
+        
+        return results
     }
     
-    private func isPalindrome(_ characters: [Character], _ start: Int, _ end: Int) -> Bool{
+    
+    func isPalindrome(_ chars: [Character], _ start: Int, _ end: Int) -> Bool{
         var start = start
         var end = end
-        while start < end{
-            if characters[start] != characters[end]{
+        
+        while(start < end){
+            if chars[start] != chars[end]{
                 return false
             }
             
             start += 1
             end -= 1
+            
         }
         
         return true
     }
 }
+
+
+//
+//class Solution {
+//    func partition(_ s: String) -> [[String]] {
+//        var results: [[String]] = []
+//        var curr: [String] = []
+//        doPartition(&curr, &results, Array(s))
+//        return results
+//
+//    }
+//
+//    private func doPartition(_ curr: inout [String], _ results: inout [[String]], _ characters: [Character]){
+//        if characters.count == 0{
+//            results.append(curr)
+//            return
+//        }
+//
+//        for i in 0..<characters.count{
+//            if isPalindrome(characters, 0, i){
+//                curr.append(String(characters[0..<i+1]))
+//                doPartition(&curr, &results, Array(characters[(i+1)...]))
+//                curr.removeLast()
+//            }
+//        }
+//    }
+//
+//    private func isPalindrome(_ characters: [Character], _ start: Int, _ end: Int) -> Bool{
+//        var start = start
+//        var end = end
+//        while start < end{
+//            if characters[start] != characters[end]{
+//                return false
+//            }
+//
+//            start += 1
+//            end -= 1
+//        }
+//
+//        return true
+//    }
+//}
 
 let solver = Solution()
 solver.partition("aab")
