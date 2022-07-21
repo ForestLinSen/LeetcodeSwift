ef4c6aa33bdcbf09a70ac598294778d333514807
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-21 14:06:33 +0800

    N-Queens solved

--- 51. N-Queens/N-Queens 2.playground/Contents.swift
+++ 51. N-Queens/N-Queens 2.playground/Contents.swift
@@ -1,6 +1,15 @@
 // https://gist.github.com/RuolinZheng08/cdd880ee748e27ed28e0be3916f56fa6
 // https://www.youtube.com/watch?v=Ph95IHmRp5M
 
+//var board = Array(repeating: Array(repeating: ".", count: 4), count: 4)
+//var join: [String] = []
+//// [".Q..","...Q","Q...","..Q."]
+//for row in board{
+//    join.append(row.joined())
+//}
+//
+//print(join)
+
 class Solution {
     
     func solveNQueens(_ n: Int) -> [[String]] {
@@ -9,17 +18,48 @@ class Solution {
         var negDiagSet = Set<Int>()
         
         var res: [[String]] = []
+        var board: [[String]] = Array(repeating: Array(repeating: ".", count: n), count: n)
         
         func backtracking(_ row: Int){
+            // add result
+            if row == n{
+                var currResult: [String] = []
+                for row in board{
+                    currResult.append(row.joined())
+                }
+                res.append(currResult)
+                return
+            }
+            
+            for col in 0..<n{
+                if colSet.contains(col) || posDiagSet.contains(row+col) || negDiagSet.contains(row-col){
+                    continue
+                }
+                
+                colSet.insert(col)
+                posDiagSet.insert(row+col)
+                negDiagSet.insert(row-col)
+                board[row][col] = "Q"
+                
+                backtracking(row+1)
+                
+                // clear the board
+                board[row][col] = "."
+                colSet.remove(col)
+                posDiagSet.remove(row+col)
+                negDiagSet.remove(row-col)
+                
+            }
             
         }
         
-        
+        backtracking(0)
+        return res
     }
 }
 
 
 
 let solver = Solution()
-//solver.currentState = [1,3,0]
-solver.search(n:4)
+let res = solver.solveNQueens(8)
+print(res, res.count)
