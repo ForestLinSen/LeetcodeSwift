2dfaae436974952c34fb7cec1ab83625c083dc32
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-25 12:08:02 +0800

    64. Minimum Path Sum solution 2

--- 64. Minimum Path Sum/Minimum Path Sum.playground/Contents.swift
+++ 64. Minimum Path Sum/Minimum Path Sum.playground/Contents.swift
@@ -1,4 +1,26 @@
+// MARK: - Solution 2
 class Solution {
+    func minPathSum(_ grid: [[Int]]) -> Int {
+        var res = Array(repeating: Array(repeating: Int.max, count: grid[0].count + 1), count: grid.count + 1)
+        res[grid.count-1][grid[0].count] = 0
+        
+        for i in stride(from: grid.count-1, to: -1, by: -1){
+            for j in stride(from: grid[0].count-1, to: -1, by: -1){
+                res[i][j] = grid[i][j] + min(res[i+1][j], res[i][j+1])
+            }
+        }
+        
+        print(res)
+        return res[0][0]
+    }
+}
+
+let solver2 = Solution()
+solver2.minPathSum([[1,2,3],[4,5,6]])
+
+
+// MARK: - Solution 1
+class Solution1 {
     func minPathSum(_ grid: [[Int]]) -> Int {
         
         if (grid.count == 0 || grid[0].count == 0){
@@ -25,5 +47,5 @@ class Solution {
     }
 }
 
-let solver = Solution()
+let solver = Solution1()
 solver.minPathSum([[1,2,3],[4,5,6]])
